9efe33c3e99aec50ea8b5f013980413e94f55b57
Luick Klippel <luickklippel@Luicks-MacBook-Pro.local>
2020-11-16 07:46:51 +0100

    adding cache handler found

--- cmd/cache/cache.go
+++ cmd/cache/cache.go
@@ -1,14 +1,43 @@
 package cache
 
+type PushPullRequest struct {
+	Key string
+
+	ReturnPayload chan []byte
+	Data []byte
+	Fullfilled bool
+}
+
 type Cache struct {
-	cache map[string][]byte
+	cacheMap map[string][]byte
+	bool CacheHandlerStarted
 }
 
 func New() Cache {
   cache := Cache{make(map[string][]byte)}
+	cache.CacheHandlerStarted = false
+	go StartCacheHandler()
   return cache
 }
 
 func (cache Cache) AddKeyVal(key string, val []byte) {
-  cache.cache[key] = val
+  cache.cacheMap[key] = val
+}
+
+func (cache Cache) StartCacheHandler(req <-chan PushPullRequest) {
+	cache.CacheHandlerStarted = true
+	requestBuffer := make([1000]*PushPullRequest)
+	go func() {
+		for {
+			select {
+			case request := <-req:
+				append(cache.requestBuffer, &request)
+			}
+		}
+	}()
+
+	for request := range *requestBuffer {
+			request.Fullfilled = true
+			delete(cache.requestBuffer, &request)
+	}
 }
